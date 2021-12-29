0e28404db752ba841629215dfa12da72a95bdef5
SamuelBustamante123 <47162302+SamuelBustamante123@users.noreply.github.com>
2021-12-29 01:58:05 -0500

    Hide

--- NBASimplify/Server/Controllers/ValuesController.cs
+++ NBASimplify/Server/Controllers/ValuesController.cs
@@ -10,27 +10,23 @@ namespace BlazorApp1.Server.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        static string _address = "https://api.sportsdata.io/v3/nba/stats/json/PlayerSeasonStats/2021";
-        private string key = "b5c072b0c0084b48976bce47e70e9757";
-        private string header = "Ocp-Apim-Subscription-Key";
-
         // GET: api/<ValuesController>
         [HttpGet]
         public async Task<List<PlayerStats>> Get()
         {
-            List<PlayerStats> ?result = await GetResponse();
+            List<PlayerStats>? result = await GetResponse();
             return result;
         }
 
         private async Task<List<PlayerStats>> GetResponse()
         {
             var client = new HttpClient();
-            var request = new HttpRequestMessage(HttpMethod.Get, _address);
-            request.Headers.Add(header, key);
+            var request = new HttpRequestMessage(HttpMethod.Get, SportsDataIoGlobals.Address);
+            request.Headers.Add(SportsDataIoGlobals.Header, SportsDataIoGlobals.Key);
             var response = await client.SendAsync(request);
             using var responseStream = await response.Content.ReadAsStreamAsync();
 
-            List<PlayerStats> ?result = await JsonSerializer.DeserializeAsync<List<PlayerStats>>(responseStream);
+            List<PlayerStats>? result = await JsonSerializer.DeserializeAsync<List<PlayerStats>>(responseStream);
             return result;
         }
     }
